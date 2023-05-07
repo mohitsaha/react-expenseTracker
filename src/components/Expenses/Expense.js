2b80ddfb68100e73328cc240a72725084c4c941d
Mohit saha <52305250+mohitsaha@users.noreply.github.com>
2023-05-07 09:45:07 +0000

    fixups

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -9,8 +9,6 @@ export default function Expense(props){
     const yearHandler = (event) =>{
         setFilterYear(event.target.value);
     }
-    //const filtered_array= expenseArray.forEach((e)=>console.log(e.date+","+ e.date.getFullYear() === filterYear+","));
-    //console.log("hello world" + filtered_array);
     return (
         <div className='expenses'>
           <ExpenseFilter yearFilter={yearHandler}/>
@@ -18,31 +16,12 @@ export default function Expense(props){
           {
            expenseArray.filter((exp)=>exp.date.getFullYear() === +filterYear).map((exp)=>{
               return  (<ExpenseItem
+              key = {expenseArray.id}
               title={exp.title}
               amount={exp.amount}
               date={exp.date}
             />)})
           }
-          {/* <ExpenseItem
-            title={props.expenses[0].title}
-            amount={props.expenses[0].amount}
-            date={props.expenses[0].date}
-          />
-          <ExpenseItem
-            title={props.expenses[1].title}
-            amount={props.expenses[1].amount}
-            date={props.expenses[1].date}
-          />
-          <ExpenseItem
-            title={props.expenses[2].title}
-            amount={props.expenses[2].amount}
-            date={props.expenses[2].date}
-          />
-          <ExpenseItem
-            title={props.expenses[3].title}
-            amount={props.expenses[3].amount}
-            date={props.expenses[3].date}
-          /> */}
         </div>
       );
 };
